86bff46f2cb2ac3c5050b6ebc5fd3c38c2b93e00
saps999 <saptarshibiswasgang@gmail.com>
2022-09-02 09:37:38 +0530

    Fibonacci number

---
+++ src/FibonacciNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the range: ");
+        int c = sc.nextInt();
+        int a=0;
+        int b=1;
+        int sum=0;
+        for (int i = 0; i <c ; i++) {
+            sum = a+b;
+            a=a+b;
+            b=a-b;
+            a=a-b;
+            b=sum;
+        }
+        System.out.println("Fibonacci number of the given range is: "+sum);
+    }
+}
