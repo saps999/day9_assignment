26a16a20a68bf98d8813a8c96a00cbf229ff3226
saps999 <saptarshibiswasgang@gmail.com>
2022-09-02 17:49:32 +0530

    Armstrong number in a given range.

---
+++ src/armstrong_number_given_range.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class armstrong_number_given_range {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the starting number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the ending number: ");
+        int num2 = sc.nextInt();
+        for (int i = num1; i <=num2 ; i++) {
+            int a=i;
+            int b=a,c=a;
+            int length=0, rem, arm=0;
+            while (a!=0){
+                length+=1;
+                a=a/10;
+            }
+            while (b!=0) {
+                rem=b%10;
+                int multi=1;
+                for (int j = 0; j < length; j++) {
+                    multi*=rem;
+                }
+                arm+=multi;
+                b=b/10;
+            }
+            if (c==arm){
+                System.out.println(i+" is an armstrong number.");
+            }
+        }
+    }
+}
